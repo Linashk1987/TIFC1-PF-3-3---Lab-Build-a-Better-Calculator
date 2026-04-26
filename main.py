5e0c7871b66c36cd0f5a94cbf1f9cab813a8b963
Linashk1987 <100642048+Linashk1987@users.noreply.github.com>
2026-04-26 17:07:44 -0500

    third lab program

--- main.py
+++ main.py
@@ -1,5 +1,40 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numbers):
+    """Return the sum of a list of numbers."""
+    return sum(numbers)
 
-if __name__=="__main__":
-  main()
+
+def multiplymultiplenumbers(numbers):
+    """Return the product of a list of numbers."""
+    product = 1
+    for number in numbers:
+        product *= number
+    return product
+
+
+def isiteven(num):
+    """Return True if num is an even whole number, otherwise False."""
+    if isinstance(num, bool):
+        return False
+    if not isinstance(num, (int, float)):
+        return False
+    if num != int(num):
+        return False
+    return int(num) % 2 == 0
+
+
+def isitaninteger(num):
+    """Return True if num is an integer value, otherwise False."""
+    if isinstance(num, bool):
+        return False
+    if not isinstance(num, (int, float)):
+        return False
+    return num == int(num)
+
+
+def main():
+    print("Hello learners!")
+    print("This calculator supports addmultiplenumbers, multiplymultiplenumbers, isiteven, and isitaninteger.")
+
+
+if __name__ == "__main__":
+    main()
